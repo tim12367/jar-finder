0c4cda9401a99aa794ea968d6456f9ec32a3f868
tim12367 <tim12367@ymail.com>
2023-10-31 22:32:58 +0800

    新增歷遍方法 與 mian方法

---
+++ MainFunction.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import ParseJar
+"""
+Created on Tue Oct 31 21:50:34 2023
+
+@author: Tim
+"""
+# main 方法
+
+
+def main():
+    parser = ParseJar.ParseJar()
+    parser.doParseJarCurrentFolder()
+
+
+if __name__ == "__main__":
+    main()
---
+++ ParseJar.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import os
+"""
+Parse jar and get info
+
+Created on Tue Oct 31 21:27:37 2023
+@author: Tim
+"""
+
+
+class ParseJar():
+    '''在檔案位置抓取jar檔資料'''
+
+    def doParseJarCurrentFolder(self):
+        # 當前目錄位置
+        currentDir = os.getcwd()
+        self.doParseAllPath(currentDir)
+
+    def doParseAllPath(self, path):
+        # 遍歷當前目錄
+        for root, dir, files in os.walk(path, topdown=True):  # topdown 是否優先遍歷TOP
+            for f in files:
+                print("root:", root, "files:", f, '\n')
+
+    def doParseJar(self, path):
+        print(path)
