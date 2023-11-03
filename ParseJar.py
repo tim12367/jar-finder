3f2ee3561ef2ddac7d459142cb302000de49a0b5
tim12367 <tim12367@ymail.com>
2023-11-03 22:20:22 +0800

    add extract jar and rename function

--- ParseJar.py
+++ ParseJar.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
+import zipfile
 """
-Parse jar and get info
+解析Jar檔
 
 Created on Tue Oct 31 21:27:37 2023
 @author: Tim
@@ -9,26 +10,35 @@ Created on Tue Oct 31 21:27:37 2023
 
 
 class ParseJar():
-    '''在檔案位置抓取jar檔資料'''
-
-    def doParseJarCurrentFolder(self):
-        # 當前目錄位置
+    # 在當前位置 抓取jar檔資料
+    def parseJarCurrentFolder(self):
         currentDir = os.getcwd()
-        self.doParseAllPath(currentDir)
+        self.parseJarByFolder(currentDir)
 
-    def doParseAllPath(self, path):
-        # 遍歷當前目錄
-        for root, dir, files in os.walk(path, topdown=True):  # topdown 是否優先遍歷TOP
+    # 依照路徑 抓取jar檔資料
+    def parseJarByFolder(self, path):
+        for root, dir, files in os.walk(path, topdown=True):
             for file in files:
-                print("root:", root, "files:", file, '\n')
+                fileExtension = os.path.splitext(file)[1]
+                print("root: ", root, "\nfile: ", file, "\nfile_extension: ", fileExtension, '\n')
 
-    def doParseJar(self, path):
-        print(path)
+                # 若檔案為jar 則解壓縮
+                if ".jar" == fileExtension:
+                    fullPath = os.path.join(root, file)
+                    self.extractJar(fullPath)
 
-#main 方法
+    def extractJar(self, fullPath):
+        with zipfile.ZipFile(fullPath, 'r') as zipObj:
+            for file_info in zipObj.infolist():
+                print("JAR內容", file_info.filename)
+                # 若為檔案
+                if "/" != file_info.filename[-1]:
+                    print("是檔案", "檔案大小為 : ", file_info.file_size)
+
+# main 方法
 def main():
     parser = ParseJar()
-    parser.doParseJarCurrentFolder()
+    parser.parseJarCurrentFolder()
 
 
 if __name__ == "__main__":
